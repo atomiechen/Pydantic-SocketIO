30baa6c0b083f9db91e9fdbe2caf8c5c86415136
Atomie CHEN <atomic_cwh@163.com>
2025-03-16 17:53:16 +0800

    perf(FastAPISocketIO): late instantiation of sio_app to allow changing socketio_path

--- src/pydantic_socketio/fastapi_socketio.py
+++ src/pydantic_socketio/fastapi_socketio.py
@@ -21,7 +21,6 @@ class FastAPISocketIO(AsyncServer):
     ) -> None:
         # disable socketio CORS handling and let fastapi CORS handle it
         super().__init__(cors_allowed_origins=[], async_mode="asgi", **kwargs)
-        self.sio_app = ASGIApp(socketio_server=self, socketio_path=socketio_path)
         self.socketio_path = socketio_path
         if app:
             self.integrate(app)
@@ -35,6 +34,7 @@ class FastAPISocketIO(AsyncServer):
                 "FastAPI is not installed. Please install FastAPI to use FastAPISocketIO."
             )
         assert isinstance(app, FastAPI), "app must be a FastAPI instance"
+        self.sio_app = ASGIApp(socketio_server=self, socketio_path=self.socketio_path)
         app.mount("/" + self.socketio_path, self.sio_app)
         app.state.sio = self
         self.fastapi_app = app
