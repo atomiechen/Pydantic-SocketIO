f4156c1a86e7e851cd2d468ce576fbd0daf457b1
Atomie CHEN <atomic_cwh@163.com>
2025-06-22 14:18:42 +0800

    feat!: support emit event data type validation

--- src/pydantic_socketio/__init__.py
+++ src/pydantic_socketio/__init__.py
@@ -2,10 +2,8 @@
 from socketio import *  # type: ignore # noqa: F403
 
 from .pydantic_socketio import (
-    BaseClient as BaseClient,
     Client as Client,
     AsyncClient as AsyncClient,
-    BaseServer as BaseServer,
     Server as Server,
     AsyncServer as AsyncServer,
     monkey_patch as monkey_patch,
--- src/pydantic_socketio/pydantic_socketio.py
+++ src/pydantic_socketio/pydantic_socketio.py
@@ -1,9 +1,9 @@
 import functools
 import inspect
 import logging
-from typing import Any, Callable, Optional, Union
+from typing import Any, Callable, Optional, Type, overload
 
-from pydantic import validate_call, ValidationError
+from pydantic import TypeAdapter, validate_call, ValidationError
 from pydantic_core import to_jsonable_python
 from socketio import (
     AsyncServer as OldAsyncServer,
@@ -17,10 +17,19 @@ from socketio.base_client import BaseClient as OldBaseClient
 
 # Save the original functions
 _old_server_on = OldBaseServer.on
+
+_old_server_init = OldServer.__init__
 _old_server_emit = OldServer.emit
+
+_old_server_init_async = OldAsyncServer.__init__
 _old_server_emit_async = OldAsyncServer.emit
+
 _old_client_on = OldBaseClient.on
+
+_old_client_init = OldClient.__init__
 _old_client_emit = OldClient.emit
+
+_old_client_init_async = OldAsyncClient.__init__
 _old_client_emit_async = OldAsyncClient.emit
 
 
@@ -31,7 +40,7 @@ module_logger.addHandler(logging.NullHandler())
 def _wrapper(
     handler: Callable,
     old_on: Callable,
-    self: Union[OldBaseClient, OldBaseServer],
+    self,
     event: str,
     *args,
     **kwargs,
@@ -65,11 +74,38 @@ def _wrapper(
     return wrapped_handler
 
 
-class BaseServer(OldBaseServer):
-    """BaseServer with pydantic validation."""
+class PydanticSioToolset:
+    """A toolset for pydantic validation and conversion for socketio."""
+
+    def __init__(self, old_on: Callable):
+        self._EMIT_EVENT_TYPES = {}
+        self._old_on = old_on
+
+    def register_emit(self, event: str, payload_type: Optional[Type] = None):
+        """Decorator to register the payload type for an event."""
+
+        def decorator(payload_type: Type):
+            self._EMIT_EVENT_TYPES[event] = payload_type
+            return payload_type
+
+        if payload_type is None:
+            # invoked as a decorator
+            return decorator
+        else:
+            # not invoked as a decorator, but as a function
+            return decorator(payload_type)
+
+    def validate_emit(self, event: str, data: Any):
+        """Validate the emit data type for the given event."""
+        expected_type = self._EMIT_EVENT_TYPES.get(event)
+        if expected_type is None:
+            # If no type is registered, skip validation
+            return
+
+        TypeAdapter(expected_type).validate_python(data)
 
     def on(
-        self: OldBaseServer,
+        self,
         event: str,
         handler: Optional[Callable] = None,
         *args,
@@ -79,7 +115,7 @@ class BaseServer(OldBaseServer):
             # invoked as a decorator
             return functools.partial(
                 _wrapper,
-                old_on=_old_server_on,
+                old_on=self._old_on,
                 self=self,
                 event=event,
                 *args,
@@ -87,77 +123,145 @@ class BaseServer(OldBaseServer):
             )
         else:
             # not invoked as a decorator, but as a function
-            return _wrapper(handler, _old_server_on, self, event, *args, **kwargs)
+            return _wrapper(handler, self._old_on, self, event, *args, **kwargs)
+
+    def schema(self):
+        """Return the event schema of the server."""
+        # TODO
+        pass
 
 
-class Server(BaseServer, OldServer):
+class Server(PydanticSioToolset, OldServer):
     """Server with pydantic validation and data conversion."""
 
+    @overload
+    def __init__(
+        self,
+        client_manager=None,
+        logger=False,
+        serializer="default",
+        json=None,
+        async_handlers=True,
+        always_connect=False,
+        namespaces=None,
+        **kwargs,
+    ): ...
+
+    @overload
+    def __init__(self, *args, **kwargs): ...
+
+    def __init__(self, *args, **kwargs):
+        _old_server_init(self, *args, **kwargs)
+        PydanticSioToolset.__init__(self, _old_server_on)
+
     def emit(
-        self: OldServer,
+        self,
         event: str,
         data: Any = None,
         to: Optional[str] = None,
         *args,
         **kwargs,
     ):
+        self.validate_emit(event, data)
         return _old_server_emit(
             self, event=event, data=to_jsonable_python(data), to=to, *args, **kwargs
         )
 
 
-class AsyncServer(BaseServer, OldAsyncServer):
+class AsyncServer(PydanticSioToolset, OldAsyncServer):
     """AsyncServer with pydantic validation and data conversion."""
 
+    @overload
+    def __init__(
+        self,
+        client_manager=None,
+        logger=False,
+        serializer="default",
+        json=None,
+        async_handlers=True,
+        always_connect=False,
+        namespaces=None,
+        **kwargs,
+    ): ...
+
+    @overload
+    def __init__(self, *args, **kwargs): ...
+
+    def __init__(self, *args, **kwargs):
+        _old_server_init_async(self, *args, **kwargs)
+        PydanticSioToolset.__init__(self, _old_server_on)
+
     async def emit(
-        self: OldAsyncServer,
+        self,
         event: str,
         data: Any = None,
         to: Optional[str] = None,
         *args,
         **kwargs,
     ):
+        self.validate_emit(event, data)
         return await _old_server_emit_async(
             self, event=event, data=to_jsonable_python(data), to=to, *args, **kwargs
         )
 
 
-class BaseClient(OldBaseClient):
-    """BaseClient with pydantic validation."""
-
-    def on(
-        self: OldBaseClient,
-        event: str,
-        handler: Optional[Callable] = None,
-        *args,
-        **kwargs,
-    ) -> Callable:
-        if handler is None:
-            # invoked as a decorator
-            return functools.partial(
-                _wrapper,
-                old_on=_old_client_on,
-                self=self,
-                event=event,
-                *args,
-                **kwargs,
-            )
-        else:
-            # not invoked as a decorator, but as a function
-            return _wrapper(handler, _old_client_on, self, event, *args, **kwargs)
-
-
-class Client(BaseClient, OldClient):
+class Client(PydanticSioToolset, OldClient):
     """Client with pydantic validation and data conversion."""
 
-    def emit(self: OldClient, event: str, data: Any = None, *args, **kwargs):
+    @overload
+    def __init__(
+        self,
+        reconnection=True,
+        reconnection_attempts=0,
+        reconnection_delay=1,
+        reconnection_delay_max=5,
+        randomization_factor=0.5,
+        logger=False,
+        serializer="default",
+        json=None,
+        handle_sigint=True,
+        **kwargs,
+    ): ...
+
+    @overload
+    def __init__(self, *args, **kwargs): ...
+
+    def __init__(self, *args, **kwargs):
+        _old_client_init(self, *args, **kwargs)
+        PydanticSioToolset.__init__(self, _old_client_on)
+
+    def emit(self, event: str, data: Any = None, *args, **kwargs):
+        self.validate_emit(event, data)
         return _old_client_emit(self, event, to_jsonable_python(data), *args, **kwargs)
 
 
-class AsyncClient(BaseClient, OldAsyncClient):
+class AsyncClient(PydanticSioToolset, OldAsyncClient):
     """AsyncClient with pydantic validation and data conversion."""
 
-    async def emit(self: OldAsyncClient, event: str, data: Any = None, *args, **kwargs):
+    @overload
+    def __init__(
+        self,
+        reconnection=True,
+        reconnection_attempts=0,
+        reconnection_delay=1,
+        reconnection_delay_max=5,
+        randomization_factor=0.5,
+        logger=False,
+        serializer="default",
+        json=None,
+        handle_sigint=True,
+        **kwargs,
+    ): ...
+
+    @overload
+    def __init__(self, *args, **kwargs): ...
+
+    def __init__(self, *args, **kwargs):
+        _old_client_init_async(self, *args, **kwargs)
+        PydanticSioToolset.__init__(self, _old_client_on)
+
+    async def emit(self, event: str, data: Any = None, *args, **kwargs):
+        self.validate_emit(event, data)
         return await _old_client_emit_async(
             self, event, to_jsonable_python(data), *args, **kwargs
         )
@@ -165,10 +269,33 @@ class AsyncClient(BaseClient, OldAsyncClient):
 
 def monkey_patch():
     module_logger.debug("Monkey patching")
-    OldBaseServer.on = BaseServer.on
-    OldServer.emit = Server.emit
-    OldAsyncServer.emit = AsyncServer.emit
-    OldBaseClient.on = BaseClient.on
-    OldClient.emit = Client.emit
-    OldAsyncClient.emit = AsyncClient.emit
+
+    setattr(OldServer, "__init__", Server.__init__)
+    setattr(OldServer, "on", Server.on)
+    setattr(OldServer, "emit", Server.emit)
+    setattr(OldServer, "register_emit", Server.register_emit)
+    setattr(OldServer, "validate_emit", Server.validate_emit)
+    setattr(OldServer, "schema", Server.schema)
+
+    setattr(OldAsyncServer, "__init__", AsyncServer.__init__)
+    setattr(OldAsyncServer, "on", AsyncServer.on)
+    setattr(OldAsyncServer, "emit", AsyncServer.emit)
+    setattr(OldAsyncServer, "register_emit", AsyncServer.register_emit)
+    setattr(OldAsyncServer, "validate_emit", AsyncServer.validate_emit)
+    setattr(OldAsyncServer, "schema", AsyncServer.schema)
+
+    setattr(OldClient, "__init__", Client.__init__)
+    setattr(OldClient, "on", Client.on)
+    setattr(OldClient, "emit", Client.emit)
+    setattr(OldClient, "register_emit", Client.register_emit)
+    setattr(OldClient, "validate_emit", Client.validate_emit)
+    setattr(OldClient, "schema", Client.schema)
+
+    setattr(OldAsyncClient, "__init__", AsyncClient.__init__)
+    setattr(OldAsyncClient, "on", AsyncClient.on)
+    setattr(OldAsyncClient, "emit", AsyncClient.emit)
+    setattr(OldAsyncClient, "register_emit", AsyncClient.register_emit)
+    setattr(OldAsyncClient, "validate_emit", AsyncClient.validate_emit)
+    setattr(OldAsyncClient, "schema", AsyncClient.schema)
+
     module_logger.debug("Monkey patched")
